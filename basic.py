01e91cdb123c0d0c0a33763db3b7138988f7ac20
Mathweuzz <mathweuzz@gmail.com>
2023-02-23 11:57:24 -0300

    Update basic.py

--- basic.py
+++ basic.py
@@ -87,7 +87,7 @@ class Position:
 # TOKENS
 #######################################
 
-TT_INT			= 'INT'
+TT_INT		= 'INT'
 TT_FLOAT    = 'FLOAT'
 TT_PLUS     = 'PLUS'
 TT_MINUS    = 'MINUS'
@@ -95,7 +95,7 @@ TT_MUL      = 'MUL'
 TT_DIV      = 'DIV'
 TT_LPAREN   = 'LPAREN'
 TT_RPAREN   = 'RPAREN'
-TT_EOF			= 'EOF'
+TT_EOF		= 'EOF'
 
 class Token:
 	def __init__(self, type_, value=None, pos_start=None, pos_end=None):
